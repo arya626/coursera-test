5b04e6f22533e562bb5172eab3e7227dcf7a2818
arya626 <aryagv626jv@gmail.com>
2020-09-07 20:57:36 +0530

    Ajax

--- scripts/script.js
+++ scripts/script.js
@@ -144,32 +144,60 @@
 // })();
 
 // console.log(document.getElementById("title"));
-document.addEventListener("DOMContentLoaded",
-    function (event){
-        function sayHello(event){
-            this.textContent ="I just said!"
-            var name = document.getElementById("name").value;
-            var message = "<h2>Hello "+ name + "!<h2>";
-        
-            // document.getElementById("content").textContent = message;
-            document.getElementById("content").innerHTML = message;
-        
-            if (name === "student"){
-                var title = document.querySelector("#title").textContent;
-                title += " & Lovin' it!";
-                document.querySelector("#title").textContent = title;
-            }
-        }
-        
-        //Unobstructive event binding
-        document.querySelector("button").addEventListener("click",sayHello);
 
-        document.querySelector("body").addEventListener("mousemove",
-            function (event){
+///////////////////////////////
+// document.addEventListener("DOMContentLoaded",
+//     function (event){
+//         function sayHello(event){
+//             this.textContent ="I just said!"
+//             var name = document.getElementById("name").value;
+//             var message = "<h2>Hello "+ name + "!<h2>";
+        
+//             // document.getElementById("content").textContent = message;
+//             document.getElementById("content").innerHTML = message;
+        
+//             if (name === "student"){
+//                 var title = document.querySelector("#title").textContent;
+//                 title += " & Lovin' it!";
+//                 document.querySelector("#title").textContent = title;
+//             }
+//         }
+        
+//         //Unobstructive event binding
+//         document.querySelector("button").addEventListener("click",sayHello);
+
+//         document.querySelector("body").addEventListener("mousemove",
+//             function (event){
                
-                console.log("x :" + event.clientX);
-                console.log("y :" + event.clientY);
+//                 console.log("x :" + event.clientX);
+//                 console.log("y :" + event.clientY);
+//             }
+//         )
+//     }
+// );
+
+document.addEventListener("DOMContentLoaded",
+function(event){
+    //unobstructive event binding
+    document.querySelector("button").addEventListener("click", function(){
+
+        //Call server to get the name
+        $ajaxUtils.sendGetRequest("data/name.json",
+        function(res){
+            var message = res.firstName + " "+ res.lastName
+            if( res.likesChineseFood){
+                message += " likes Chinese food";
             }
-        )
-    }
+            else{
+                message += " doesn't like Chinese food";
+            }
+            message += " and uses ";
+            message += res.numberOfDisplays;
+            message += " displays for coding";
+
+            document.querySelector("#content").innerHTML = "<h2>"+message+
+            "</h2>";
+        });
+    });
+}
 );
